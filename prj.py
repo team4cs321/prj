7f78f188a8719be5243f6badd1717e266dcbcbca
Reza Ahmad <reza@reza-mbp.corp.dropbox.com>
2015-11-28 13:39:54 -0500

    counts

--- prj.py
+++ prj.py
@@ -1,13 +1,23 @@
-import sys
+from collections import Counter
 import os
+import pprint
+import sys
 
 
 
+positive_reviews = []
+negative_reviews = []
 
 def naivebayes(data, reviewtype=None):
 	if reviewtype is None:
 		print('Nice try, brown!')
-	return
+		return
+
+	elif reviewtype is 'positive':
+		positive_reviews.append((data, '+'))
+
+
+
 
 
 def main(argv):
@@ -23,7 +33,14 @@ def main(argv):
 		for review in files:
 			# we'll need to feed these each into naive bayes
 			# naivebayes(review, positive)
-			print(os.path.join(root, review))
+			filename = os.path.join(root, review)
+			#print(os.path.join(root, review))
+			print(filename)
+			text = open(filename, 'r')
+			text = text.read()
+			text = text.replace('<br />', '')
+			#print(text)
+			naivebayes(text, 'positive')
 
 
 	print('read and processed positive reviews\n')
@@ -33,17 +50,30 @@ def main(argv):
 	for root, dirs, files, in os.walk(neg, topdown=True):
 		for review in files:
 			# we'll need to feed these each into naive bayes
-			# naivebayes(review, positive)
+			# naivebayes(review, negative)
 			print(os.path.join(root, review))
 
 
 	print('read and processed negative reviews\n')
 
+	clean = []
+	for (text, review) in positive_reviews:
+		cleanup = [word.lower() for word in text.split() if len(word) > 2]
+		clean.append((cleanup, review))
 
-	text_to_review = raw_input('Input a review to be rated: ')
-	sentiment = naivebayes(text_to_review, None)
+	#pprint.pprint(clean)
 
+	blah = []
+	for (derp, rev) in clean:
+		blah.extend(derp)
 
+	a = Counter(blah)
+	pprint.pprint(a)
+
+	#text_to_review = raw_input('Input a review to be rated: ')
+	#sentiment = naivebayes(text_to_review, None)
+
+	#pprint.pprint(positive_reviews)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
